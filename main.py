a0f4cfebbc6697088ce8e247059bffbab49f2672
Laz627 <169095963+Laz627@users.noreply.github.com>
2024-07-03 19:02:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-####################################################################################
-# Simple GSC Connector for Streamlit by Lee Foot 12th June 2024                    #
-# Website  : https://leefoot.co.uk/                                                #
-# Contact  : https://leefoot.co.uk/hire-me/                                        #
-# LinkedIn : https://www.linkedin.com/in/lee-foot/                                 #
-# Twitter  : https://twitter.com/LeeFootSEO                                        #
-####################################################################################
-
 # Standard library imports
 import datetime
 import base64
 
 # Related third-party imports
 import streamlit as st
-from streamlit_elements import Elements
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 import pandas as pd
 import searchconsole
 
 # Configuration: Set to True if running locally, False if running on Streamlit Cloud
-# IS_LOCAL = True
 IS_LOCAL = False
 
 # Constants
@@ -38,36 +28,26 @@ BASE_DIMENSIONS = ["page", "query", "country", "date"]
 MAX_ROWS = 250_000
 DF_PREVIEW_ROWS = 100
 
-
 # -------------
 # Streamlit App Configuration
 # -------------
 
 def setup_streamlit():
-    """
-    Configures Streamlit's page settings and displays the app title and markdown information.
-    Sets the page layout, title, and markdown content with links and app description.
-    """
-    st.set_page_config(page_title="✨ Simple Google Search Console Data | LeeFoot.co.uk", layout="wide")
-    st.title("✨ Simple Google Search Console Data | June 2024")
+    st.set_page_config(page_title="✨ Simple Google Search Console Data | YourApp", layout="wide")
+    st.title("✨ Simple Google Search Console Data | July 2024")
     st.markdown(f"### Lightweight GSC Data Extractor. (Max {MAX_ROWS:,} Rows)")
 
     st.markdown(
         """
         <p>
-            Created by <a href="https://twitter.com/LeeFootSEO" target="_blank">LeeFootSEO</a> |
-            <a href="https://leefoot.co.uk" target="_blank">More Apps & Scripts on my Website</a>
+            Created by <a href="https://twitter.com/YourTwitterHandle" target="_blank">YourName</a> |
+            <a href="https://yourwebsite.com" target="_blank">More Apps & Scripts on my Website</a>
         """,
         unsafe_allow_html=True
     )
     st.divider()
 
-
 def init_session_state():
-    """
-    Initialises or updates the Streamlit session state variables for property selection,
-    search type, date range, dimensions, and device type.
-    """
     if 'selected_property' not in st.session_state:
         st.session_state.selected_property = None
     if 'selected_search_type' not in st.session_state:
@@ -87,60 +67,40 @@ def init_session_state():
     if 'custom_end_date' not in st.session_state:
         st.session_state.custom_end_date = datetime.date.today()
 
-
 # -------------
 # Google Authentication Functions
 # -------------
 
 def load_config():
-    """
-    Loads the Google API client configuration from Streamlit secrets.
-    Returns a dictionary with the client configuration for OAuth.
-    """
     client_config = {
         "installed": {
-            "client_id": str(st.secrets["installed"]["client_id"]),
-            "client_secret": str(st.secrets["installed"]["client_secret"]),
+            "client_id": st.secrets["oauth"]["client_id"],
+            "client_secret": st.secrets["oauth"]["client_secret"],
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://accounts.google.com/o/oauth2/token",
             "redirect_uris": (
                 ["http://localhost:8501"]
                 if IS_LOCAL
-                else [str(st.secrets["installed"]["redirect_uris"][0])]
+                else ["https://search-console-query-api.streamlit.app"]
             ),
         }
     }
     return client_config
 
-
 def init_oauth_flow(client_config):
-    """
-    Initialises the OAuth flow for Google API authentication using the client configuration.
-    Sets the necessary scopes and returns the configured Flow object.
-    """
-    scopes = ["https://www.googleapis.com/auth/webmasters"]
+    scopes = ["https://www.googleapis.com/auth/webmasters.readonly"]
     return Flow.from_client_config(
         client_config,
         scopes=scopes,
         redirect_uri=client_config["installed"]["redirect_uris"][0],
     )
 
-
 def google_auth(client_config):
-    """
-    Starts the Google authentication process using OAuth.
-    Generates and returns the OAuth flow and the authentication URL.
-    """
     flow = init_oauth_flow(client_config)
     auth_url, _ = flow.authorization_url(prompt="consent")
     return flow, auth_url
 
-
 def auth_search_console(client_config, credentials):
-    """
-    Authenticates the user with the Google Search Console API using provided credentials.
-    Returns an authenticated searchconsole client.
-    """
     token = {
         "token": credentials.token,
         "refresh_token": credentials.refresh_token,
@@ -152,26 +112,16 @@ def auth_search_console(client_config, credentials):
     }
     return searchconsole.authenticate(client_config=client_config, credentials=token)
 
-
 # -------------
 # Data Fetching Functions
 # -------------
 
 def list_gsc_properties(credentials):
-    """
-    Lists all Google Search Console properties accessible with the given credentials.
-    Returns a list of property URLs or a message if no properties are found.
-    """
     service = build('webmasters', 'v3', credentials=credentials)
     site_list = service.sites().list().execute()
     return [site['siteUrl'] for site in site_list.get('siteEntry', [])] or ["No properties found"]
 
-
 def fetch_gsc_data(webproperty, search_type, start_date, end_date, dimensions, device_type=None):
-    """
-    Fetches Google Search Console data for a specified property, date range, dimensions, and device type.
-    Handles errors and returns the data as a DataFrame.
-    """
     query = webproperty.query.range(start_date, end_date).search_type(search_type).dimension(*dimensions)
 
     if 'device' in dimensions and device_type and device_type != 'All Devices':
@@ -183,33 +133,18 @@ def fetch_gsc_data(webproperty, search_type, start_date, end_date, dimensions, d
         show_error(e)
         return pd.DataFrame()
 
-
 def fetch_data_loading(webproperty, search_type, start_date, end_date, dimensions, device_type=None):
-    """
-    Fetches Google Search Console data with a loading indicator. Utilises 'fetch_gsc_data' for data retrieval.
-    Returns the fetched data as a DataFrame.
-    """
     with st.spinner('Fetching data...'):
         return fetch_gsc_data(webproperty, search_type, start_date, end_date, dimensions, device_type)
-
 
 # -------------
 # Utility Functions
 # -------------
 
 def update_dimensions(selected_search_type):
-    """
-    Updates and returns the list of dimensions based on the selected search type.
-    Adds 'device' to dimensions if the search type requires it.
-    """
     return BASE_DIMENSIONS + ['device'] if selected_search_type in SEARCH_TYPES else BASE_DIMENSIONS
 
-
 def calc_date_range(selection, custom_start=None, custom_end=None):
-    """
-    Calculates the date range based on the selected range option.
-    Returns the start and end dates for the specified range.
-    """
     range_map = {
         'Last 7 Days': 7,
         'Last 30 Days': 30,
@@ -226,40 +161,21 @@ def calc_date_range(selection, custom_start=None, custom_end=None):
             return today - datetime.timedelta(days=7), today
     return today - datetime.timedelta(days=range_map.get(selection, 0)), today
 
-
 def show_error(e):
-    """
-    Displays an error message in the Streamlit app.
-    Formats and shows the provided error 'e'.
-    """
     st.error(f"An error occurred: {e}")
 
-
 def property_change():
-    """
-    Updates the 'selected_property' in the Streamlit session state.
-    Triggered on change of the property selection.
-    """
     st.session_state.selected_property = st.session_state['selected_property_selector']
-
 
 # -------------
 # File & Download Operations
 # -------------
 
 def show_dataframe(report):
-    """
-    Shows a preview of the first 100 rows of the report DataFrame in an expandable section.
-    """
     with st.expander("Preview the First 100 Rows"):
         st.dataframe(report.head(DF_PREVIEW_ROWS))
 
-
 def download_csv_link(report):
-    """
-    Generates and displays a download link for the report DataFrame in CSV format.
-    """
-
     def to_csv(df):
         return df.to_csv(index=False, encoding='utf-8-sig')
 
@@ -268,27 +184,17 @@ def download_csv_link(report):
     href = f'<a href="data:file/csv;base64,{b64_csv}" download="search_console_data.csv">Download CSV File</a>'
     st.markdown(href, unsafe_allow_html=True)
 
-
 # -------------
 # Streamlit UI Components
 # -------------
 
 def show_google_sign_in(auth_url):
-    """
-    Displays the Google sign-in button and authentication URL in the Streamlit sidebar.
-    """
     with st.sidebar:
         if st.button("Sign in with Google"):
-            # Open the authentication URL
             st.write('Please click the link below to sign in:')
             st.markdown(f'[Google Sign-In]({auth_url})', unsafe_allow_html=True)
 
-
 def show_property_selector(properties, account):
-    """
-    Displays a dropdown selector for Google Search Console properties.
-    Returns the selected property's webproperty object.
-    """
     selected_property = st.selectbox(
         "Select a Search Console Property:",
         properties,
@@ -299,12 +205,7 @@ def show_property_selector(properties, account):
     )
     return account[selected_property]
 
-
 def show_search_type_selector():
-    """
-    Displays a dropdown selector for choosing the search type.
-    Returns the selected search type.
-    """
     return st.selectbox(
         "Select Search Type:",
         SEARCH_TYPES,
@@ -312,12 +213,7 @@ def show_search_type_selector():
         key='search_type_selector'
     )
 
-
 def show_date_range_selector():
-    """
-    Displays a dropdown selector for choosing the date range.
-    Returns the selected date range option.
-    """
     return st.selectbox(
         "Select Date Range:",
         DATE_RANGE_OPTIONS,
@@ -325,21 +221,11 @@ def show_date_range_selector():
         key='date_range_selector'
     )
 
-
 def show_custom_date_inputs():
-    """
-    Displays date input fields for custom date range selection.
-    Updates session state with the selected dates.
-    """
     st.session_state.custom_start_date = st.date_input("Start Date", st.session_state.custom_start_date)
     st.session_state.custom_end_date = st.date_input("End Date", st.session_state.custom_end_date)
 
-
 def show_dimensions_selector(search_type):
-    """
-    Displays a multi-select box for choosing dimensions based on the selected search type.
-    Returns the selected dimensions.
-    """
     available_dimensions = update_dimensions(search_type)
     return st.multiselect(
         "Select Dimensions:",
@@ -348,12 +234,7 @@ def show_dimensions_selector(search_type):
         key='dimensions_selector'
     )
 
-
 def show_fetch_data_button(webproperty, search_type, start_date, end_date, selected_dimensions):
-    """
-    Displays a button to fetch data based on selected parameters.
-    Shows the report DataFrame and download link upon successful data fetching.
-    """
     if st.button("Fetch Data"):
         report = fetch_data_loading(webproperty, search_type, start_date, end_date, selected_dimensions)
 
@@ -361,17 +242,11 @@ def show_fetch_data_button(webproperty, search_type, start_date, end_date, selec
             show_dataframe(report)
             download_csv_link(report)
 
-
 # -------------
 # Main Streamlit App Function
 # -------------
 
-# Main Streamlit App Function
 def main():
-    """
-    The main function for the Streamlit application.
-    Handles the app setup, authentication, UI components, and data fetching logic.
-    """
     setup_streamlit()
     client_config = load_config()
     st.session_state.auth_flow, st.session_state.auth_url = google_auth(client_config)
@@ -403,7 +278,6 @@ def main():
 
             selected_dimensions = show_dimensions_selector(search_type)
             show_fetch_data_button(webproperty, search_type, start_date, end_date, selected_dimensions)
-
 
 if __name__ == "__main__":
     main()
